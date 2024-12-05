b3bd25dea5f27cafabd4905de9bfca785d55bb4f
ArvinoDel <andikasupriyadi27@gmail.com>
2024-12-05 09:49:12 +0700

    asdasdsad

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,7 +34,9 @@ class AppServiceProvider extends ServiceProvider
             return "<?php echo asset($path); ?>";
         });
 
-        // URL::forceRootUrl('https://pandaidigital.id');
+        $this->app['request']->server->set('HTTPS', true);
+        URL::forceRootUrl('https://pandaidigital.id');
+        URL::forceScheme('https');
             // dd($path);
 
          // Ambil data identitas website dari tabel Identitaswebsite
