faecd798541354448779e63690ab1f979731c6a6
ArvinoDel <andikasupriyadi27@gmail.com>
2024-12-05 09:38:49 +0700

    asdfsdaf

--- .trash/resources/views/administrator/layout.blade.php
+++ .trash/resources/views/administrator/layout.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
   <title>Welcome Administrator-</title>
